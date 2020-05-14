601256a512a4dc15d5ef8d5b01e7104d6822942e
karloCode <karloCode96@gmail.com>
2020-05-14 21:11:07 +0800

    git commit final

--- development/main.8f75cf09.js
+++ development/main.8f75cf09.js
@@ -232,11 +232,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64637" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64570" + '/');
->>>>>>> navbarArea
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64758" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

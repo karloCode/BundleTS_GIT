56d2755388c662741de0f0012b21ffea032d6582
karloCode <karloCode96@gmail.com>
2020-05-14 21:45:38 +0800

    dfsd

--- development/main.8f75cf09.js
+++ development/main.8f75cf09.js
@@ -409,7 +409,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49168" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49588" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
